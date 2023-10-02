0751a219d9aeb96b87c68c606404613097d04113
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2023-10-02 17:18:27 +0300

    Sorting query results

---
+++ iTour/DestinatonListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinatonListingView.swift
+//  iTour
+//
+//  Created by Ramill Ibragimov on 10/2/23.
+//
+
+import SwiftUI
+
+struct DestinatonListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinatonListingView()
+}
